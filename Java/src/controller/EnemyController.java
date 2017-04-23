3683a6db4a0814734a2d9f6a38de4d8c89f3099e
vincenthend <vincenthendrha@gmail.com>
2017-04-24 00:07:47 +0700

    (24/04) Model Finished

---
+++ Java/src/controller/EnemyController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import model.entity.EnemyEntity;
+
+public class EnemyController extends Thread {
+  EnemyEntity enemyEntity;
+  boolean isRunning;
+
+  public EnemyController(EnemyEntity enemyEntity) {
+
+  }
+
+  public void run() {
+    //while isrunning
+    //sleep for ... seconds
+    //jalan
+  }
+}
---
+++ Java/src/controller/PlayerController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import java.awt.event.KeyListener;
+import model.entity.PlayerEntity;
+import model.player.Player;
+
+public class PlayerController extends Thread {
+  private PlayerEntity player;
+  private KeyListener key;
+  private boolean isRunning;
+
+  public PlayerController(Player player, KeyListener keyListener) {
+
+  }
+
+  @Override
+  public void run() {
+    //while isrunning
+    //wait for keystroke
+      //jalan
+  }
+}
