ee7fbdb04e4d95b1515a4b95fb21a647ebd03fd2
vincenthend <vincenthendrha@gmail.com>
2017-04-25 00:31:31 +0700

    (24/04)Battle needed

--- Java/src/controller/PlayerController.java
+++ Java/src/controller/PlayerController.java
@@ -1,23 +1,19 @@
 package controller;
 
-import model.entity.Entity;
-import model.entity.PlayerEntity;
-import model.map.Cell;
-import model.map.Map;
-import model.map.TransferPoint;
-import view.GameInterface;
-
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import model.entity.Entity;
+import model.entity.PlayerEntity;
+import model.map.Map;
+import view.GameInterface;
 
 public class PlayerController implements KeyListener {
-
   private PlayerEntity playerEntity;
   private GameInterface gameInterface;
   private int key;
   private Map map;
 
-  public PlayerController(PlayerEntity playerEntity, GameInterface gameInterface, Map map){
+  public PlayerController(PlayerEntity playerEntity, GameInterface gameInterface, Map map) {
     this.playerEntity = playerEntity;
     this.gameInterface = gameInterface;
     this.map = map;
@@ -29,98 +25,41 @@ public class PlayerController implements KeyListener {
 
   @Override
   public void keyPressed(KeyEvent keyEvent) {
-    Cell tempCell;
-    TransferPoint tempTP;
-    key = keyEvent.getKeyCode();
-    if (key == KeyEvent.VK_LEFT) {
-      if (map.isInRange(playerEntity.getPosition(Entity.LEFT))){
-        if (map.getMapCell(playerEntity.getPosition(Entity.LEFT)).getTerrain()
-                .isPassable()) {
-          tempCell = map.getMapCell(playerEntity.getPosition());
-          tempCell.setEntity(null);
-          tempCell = map.getMapCell(playerEntity.getPosition(Entity.LEFT));
-          tempCell.setEntity(playerEntity);
-          playerEntity.move(Entity.LEFT);
-        }
-      }
-      else {
-        tempCell = map.getMapCell(playerEntity.getPosition());
-        tempCell.setEntity(null);
-        tempTP = map.getTransferPoint(playerEntity.getPosition());
-        playerEntity.setPosition(tempTP.getExitPoint());
-        map = tempTP.getNextMap();
-        tempCell = map.getMapCell(playerEntity.getPosition());
-        tempCell.setEntity(playerEntity);
-      }
-    }
-    else if (key == KeyEvent.VK_RIGHT) {
-      if (map.isInRange(playerEntity.getPosition(Entity.RIGHT))){
-        if (map.getMapCell(playerEntity.getPosition(Entity.RIGHT)).getTerrain()
-                .isPassable()) {
-          tempCell = map.getMapCell(playerEntity.getPosition());
-          tempCell.setEntity(null);
-          tempCell = map.getMapCell(playerEntity.getPosition(Entity.RIGHT));
-          tempCell.setEntity(playerEntity);
-          playerEntity.move(Entity.RIGHT);
-        }
-      }
-      else {
-        tempCell = map.getMapCell(playerEntity.getPosition());
-        tempCell.setEntity(null);
-        tempTP = map.getTransferPoint(playerEntity.getPosition());
-        playerEntity.setPosition(tempTP.getExitPoint());
-        map = tempTP.getNextMap();
-        tempCell = map.getMapCell(playerEntity.getPosition());
-        tempCell.setEntity(playerEntity);
-      }
-    }
-    else if (key == KeyEvent.VK_UP) {
-      if (map.isInRange(playerEntity.getPosition(Entity.UP))){
-        if (map.getMapCell(playerEntity.getPosition(Entity.UP)).getTerrain()
-                .isPassable()) {
-          tempCell = map.getMapCell(playerEntity.getPosition());
-          tempCell.setEntity(null);
-          tempCell = map.getMapCell(playerEntity.getPosition(Entity.UP));
-          tempCell.setEntity(playerEntity);
-          playerEntity.move(Entity.UP);
-        }
-      }
-      else {
-        tempCell = map.getMapCell(playerEntity.getPosition());
-        tempCell.setEntity(null);
-        tempTP = map.getTransferPoint(playerEntity.getPosition());
-        playerEntity.setPosition(tempTP.getExitPoint());
-        map = tempTP.getNextMap();
-        tempCell = map.getMapCell(playerEntity.getPosition());
-        tempCell.setEntity(playerEntity);
-      }
-    }
-    else if (key == KeyEvent.VK_DOWN) {
-      if (map.isInRange(playerEntity.getPosition(Entity.DOWN))){
-        if (map.getMapCell(playerEntity.getPosition(Entity.DOWN)).getTerrain()
-                .isPassable()) {
-          tempCell = map.getMapCell(playerEntity.getPosition());
-          tempCell.setEntity(null);
-          tempCell = map.getMapCell(playerEntity.getPosition(Entity.DOWN));
-          tempCell.setEntity(playerEntity);
-          playerEntity.move(Entity.DOWN);
-        }
-      }
-      else {
-        tempCell = map.getMapCell(playerEntity.getPosition());
-        tempCell.setEntity(null);
-        tempTP = map.getTransferPoint(playerEntity.getPosition());
-        playerEntity.setPosition(tempTP.getExitPoint());
-        map = tempTP.getNextMap();
-        tempCell = map.getMapCell(playerEntity.getPosition());
-        tempCell.setEntity(playerEntity);
-      }
-    }
-    gameInterface.updateInterface();
+
   }
 
   @Override
   public void keyReleased(KeyEvent keyEvent) {
-    key = -999;
+    int move = -1;
+
+    key = keyEvent.getKeyCode();
+    if (key == KeyEvent.VK_LEFT) {
+      move = Entity.LEFT;
+    }
+    else if (key == KeyEvent.VK_RIGHT) {
+      move = Entity.RIGHT;
+    }
+    if (key == KeyEvent.VK_UP) {
+      move = Entity.UP;
+    }
+    if (key == KeyEvent.VK_DOWN) {
+      move = Entity.DOWN;
+    }
+
+    if (move != -1) {
+      if (map.inBounds(playerEntity.getPosition(move))) {
+        if (map.getMapCell(playerEntity.getPosition(move)).getTerrain().isPassable()) {
+          map.getMapCell(playerEntity.getPosition(move)).setEntity(playerEntity);
+          map.getMapCell(playerEntity.getPosition()).setEntity(null);
+          playerEntity.move(move);
+        }
+      }
+      else {
+        //pindah map
+      }
+      System.out.println(playerEntity.getPosition());
+      gameInterface.updateMap(map);
+      gameInterface.updateInterface();
+    }
   }
 }
