ca71d4228cc9394f5cd0d79ac4910d057606bb0d
vincenthend <vincenthendrha@gmail.com>
2017-04-25 19:51:12 +0700

    (25/04) Kurang battle

--- Java/src/model/enemy/EnemyGenerator.java
+++ Java/src/model/enemy/EnemyGenerator.java
@@ -3,11 +3,15 @@ package model.enemy;
 import java.util.Random;
 
 /**
- * Class EnemyGenerator generates a random enemy
+ * Class EnemyGenerator generates a random enemy.
  */
 public class EnemyGenerator {
   public static final int countEnemy = 1;
 
+  /**
+   * Melakukan generate pada musuh secara random.
+   * @return musuh hasil random
+   */
   public Enemy generateEnemy(){
     Random rand = new Random(System.currentTimeMillis());
     int i;
