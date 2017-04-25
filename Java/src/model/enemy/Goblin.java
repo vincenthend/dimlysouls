a73337a5d03f9aff764fe9eccb59e06680b5cde4
alcreia <scizor.scythe@gmail.com>
2017-04-25 22:50:10 +0700

    Enemy types added

---
+++ Java/src/model/enemy/Goblin.java
@@ -0,0 +1,56 @@
+package model.enemy;
+
+/**
+ * Kelas musuh goblin.
+ * @author
+ */
+public class Goblin implements Enemy {
+  private int health;
+  private int attack;
+  private int defense;
+  private int speed;
+  private int expGet;
+  private String renderCode;
+
+  /**
+   * Konstruktor kelas goblin.
+   */
+  public Goblin() {
+    health = 35;
+    attack = 15;
+    defense = 15;
+    speed = 70;
+    expGet = 20;
+    renderCode = "#";
+  }
+
+  @Override
+  public int getHealth() {
+    return health;
+  }
+
+  @Override
+  public int getAttack() {
+    return attack;
+  }
+
+  @Override
+  public int getDefense() {
+    return defense;
+  }
+
+  @Override
+  public int getSpeed() {
+    return speed;
+  }
+
+  @Override
+  public int getExp() {
+    return expGet;
+  }
+
+  @Override
+  public String getRenderCode() {
+    return renderCode;
+  }
+}
---
+++ Java/src/model/enemy/Minotaur.java
@@ -0,0 +1,56 @@
+package model.enemy;
+
+/**
+ * Kelas musuh minotaur.
+ * @author
+ */
+public class Minotaur implements Enemy {
+  private int health;
+  private int attack;
+  private int defense;
+  private int speed;
+  private int expGet;
+  private String renderCode;
+
+  /**
+   * Konstruktor kelas minotaur.
+   */
+  public Minotaur() {
+    health = 100;
+    attack = 45;
+    defense = 60;
+    speed = 15;
+    expGet = 100;
+    renderCode = "!";
+  }
+
+  @Override
+  public int getHealth() {
+    return health;
+  }
+
+  @Override
+  public int getAttack() {
+    return attack;
+  }
+
+  @Override
+  public int getDefense() {
+    return defense;
+  }
+
+  @Override
+  public int getSpeed() {
+    return speed;
+  }
+
+  @Override
+  public int getExp() {
+    return expGet;
+  }
+
+  @Override
+  public String getRenderCode() {
+    return renderCode;
+  }
+}
